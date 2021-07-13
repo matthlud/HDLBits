e3c246399f37bc88d225b399e3b1ac19179e99ec
matthlud <muster.mann123@t-online.de>
2021-07-13 19:59:27 +0200

    Solutions of 01& 02.01

---
+++ 02_Verilog_Language/01_Basics/07_Declaring_wires.v
@@ -0,0 +1,18 @@
+`default_nettype none
+module top_module(
+    input a,
+    input b,
+    input c,
+    input d,
+    output out,
+    output out_n   ); 
+    
+    wire one, two, three;
+    assign one = a && b; 
+    assign two = c && d;
+    
+    assign three = one || two;
+    assign out = three;
+    assign out_n = ~three;
+    
+endmodule
