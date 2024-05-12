5b935ce1636b778cdbd14840c2679d150f968fdc
matthlud <42676592+matthlud@users.noreply.github.com>
2024-05-12 22:34:33 +0200

    Solve 03_01_01 to 03_01_13 excluding 03_01_08

---
+++ 03_Circuits/01_Combinational_Logic/01_Basic_Gates/12_Combine_circuits_A_and_B.v
@@ -0,0 +1,44 @@
+module a (input x, input y, output z);
+    assign z = (x^y) & x;
+endmodule
+
+
+module b ( input x, input y, output z );
+    assign z = !(x^y);
+endmodule
+
+module top_module (input x, input y, output z);
+    
+    wire z1, z2, z3, z4;
+    
+    a ia1
+    (
+        .x(x),
+        .y(y),
+        .z(z1),
+    );
+    
+    a ia2
+    (
+        .x(x),
+        .y(y),
+        .z(z2),
+    );
+    
+    b ib1
+    (
+        .x(x),
+        .y(y),
+        .z(z3),
+    );
+    
+    b ib2
+    (
+        .x(x),
+        .y(y),
+        .z(z4),
+    );
+    
+    assign z = ((z1|z2)^(z3&z4));
+
+endmodule
